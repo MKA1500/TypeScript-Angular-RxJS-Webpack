331c0be0537a5085954c73c6ec38098405cb57e7
MKA1500 <maciejkabsch@gmail.com>
2018-11-25 19:44:28 +0100

    RxJS

--- RXJS/src/code.ts
+++ RXJS/src/code.ts
@@ -1,3 +1,29 @@
-import * as Rx from 'rxjs/Observable';
+// npm run start
 
-console.log(Rx);
+import { Observable } from 'rxjs/Observable';
+
+var observable = Observable.create((observer:any) => {
+  try {
+    observer.next('hello 1');
+    observer.next('hello 2');
+    observer.next('hello 3');
+    observer.complete();
+    observer.next('hello not send');
+  } catch(err) {
+    observer.error(err)
+  }
+});
+
+observable.subscribe(
+  (x:any) => addItem(x),
+  (error:any) => addItem(error),
+  () => addItem('Completed')
+);
+
+function addItem(val:any) {
+  var node = document.createElement('li');
+  node.classList.add('list-group-item');
+  var textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById('output').appendChild(node);
+}
