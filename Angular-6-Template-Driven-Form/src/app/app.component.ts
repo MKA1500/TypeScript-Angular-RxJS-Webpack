6b5aaf599983ccc01ca9936bf94bbdc1d39f1dbb
MKA1500 <maciejkabsch@gmail.com>
2018-12-09 13:17:47 +0100

    ngModel, NgForm and submit

--- Angular-6-Template-Driven-Form/src/app/app.component.ts
+++ Angular-6-Template-Driven-Form/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value.username);
+    console.log(form.value.email);
+    console.log(form.value.secret);
+    console.log('form object:');
+    console.log(form);
   }
 }
